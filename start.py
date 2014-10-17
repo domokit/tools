0747db7c5705d6ce0ed86eef1fcd1b7e370f3d82
James Robinson <jamesr@chromium.org>
2014-10-17 14:29:33 -0700

    sudo -n

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import subprocess
 
 job_name = os.environ['JOB_NAME']
 
-retval = subprocess.call(["sudo", "docker", "run", "--name=%s" % job_name, "e0516f09bd70", "/mojo/build.py"])
-subprocess.check_call(["sudo", "docker", "rm", job_name])
+retval = subprocess.call(["sudo", "-n", "docker", "run", "--name=%s" % job_name, "e0516f09bd70", "/mojo/build.py"])
+subprocess.check_call(["sudo", "-n", "docker", "rm", job_name])
 
 os.exit(retval)
