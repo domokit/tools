1b900e0a1b36336fdc24ecfc6ad771c2987ce567
James Robinson <jamesr@chromium.org>
2014-10-17 14:43:30 -0700

    Add prepare_image.py, pick and print random container name

---
+++ prepare_image.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+def system(command):
+    return subprocess.check_call(command)
+
+def root(command):
+    return system(["sudo", "-n"] + command)
+
+root(["mkdir", "/ssd"])
+root(["mount", "-t", "ext4", "/dev/sdb", "/ssd"])
+root(["ln", "-s", "/ssd/docker", "/var/lib/docker"])
+
+subprocess.check_call("wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo -n apt-key add -", shell=True)
+subprocess.check_call("sudo -n sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'", shell=True)
+subprocess.check_call("curl -sSL https://get.docker.com/ | sh", shell=True)
+
+root(["apt-get", "update"])
+root(["apt-get", "install", "-y", "git", "jenkins"])
+# TODO: add user 'jenkins' to the sudo without password list
+root(["usermod", "-G", "sudo", "jenkins"])
+
+system(["git", "clone", "https://github.com/domokit/tools.git"])
+
--- start.py
+++ start.py
@@ -2,8 +2,10 @@
 
 import os
 import subprocess
+import random
 
-job_name = os.environ['JOB_NAME']
+job_name = "mojo-builder-%s" % str(random.randint(1, 100000))
+print "job_name:" job_name
 
 retval = subprocess.call(["sudo", "-n", "docker", "run", "--name=%s" % job_name, "e0516f09bd70", "/mojo/build.py"])
 subprocess.check_call(["sudo", "-n", "docker", "rm", job_name])
